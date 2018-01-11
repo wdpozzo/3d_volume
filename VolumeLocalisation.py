0734b466c2c8f5448294be220f21343d157ac832
Walter Del Pozzo <walter.delpozzo@ligo.org>
2018-01-11 09:19:50 +0100

    3D volume plot

--- VolumeLocalisation.py
+++ VolumeLocalisation.py
@@ -716,7 +716,7 @@ if __name__=='__main__':
         from mpl_toolkits.mplot3d import Axes3D
         # Create a cartesian grid
         N = 200
-        MAX = dpgmm.grid[0][-1]
+        MAX = np.max(celestial_to_cartesian(np.array([dpgmm.grid[0][-1],0.0,0.0])))
         x = np.linspace(-MAX,MAX,N)
         y = np.linspace(-MAX,MAX,N)
         z = np.linspace(-MAX,MAX,N)
@@ -728,20 +728,40 @@ if __name__=='__main__':
         # create a normalized cumulative distribution
         log_cartesian_sorted = np.sort(log_cartesian_map.flatten())[::-1]
         log_cartesian_cum = cumulative.fast_log_cumulative(log_cartesian_sorted)
-        # find the indeces  corresponding to the given CLs
+        # find the indeces corresponding to the given CLs
         adLevels = np.ravel([0.05,0.5,0.95])
         args = [(log_cartesian_sorted,log_cartesian_cum,level) for level in adLevels]
         adHeights = dpgmm.pool.map(FindHeights,args)
         heights = {str(lev):hei for lev,hei in zip(adLevels,adHeights)}
         
-        fig = plt.figure(figsize=(13.5,8))
+        fig = plt.figure(figsize=(13.5,13.5))
         ax = fig.add_subplot(111, projection='3d')
-        ax.view_init(elev=45, azim=75)
+        # draw sphere
+        u, v = np.mgrid[0:2*np.pi:100j, 0:np.pi:100j]
+        xs = MAX*np.cos(u)*np.sin(v)
+        ys = MAX*np.sin(u)*np.sin(v)
+        zs = MAX*np.cos(v)
+        ax.plot_wireframe(xs, ys, zs, color="0.9", alpha=0.5, lw=0.3)
+        # initialise the view so that the observer is orthogonal to the volume
+        ax.view_init(elev=np.mean(lat_map), azim=np.mean(lon_map))
         for lev,cmap,al in zip(['0.05','0.5','0.95'],['BrBG','YlOrBr','BuPu'],[0.9,0.3,0.1]):
-            verts, faces, normals, values = measure.marching_cubes(np.exp(log_cartesian_map), np.exp(heights[lev]), spacing=(np.diff(x)[0],np.diff(y)[0],np.diff(z)[0]))
+            verts, faces, normals, values = measure.marching_cubes_lewiner(np.exp(log_cartesian_map), np.exp(heights[lev]), spacing=(np.diff(x)[0],np.diff(y)[0],np.diff(z)[0]))
             ax.plot_trisurf(verts[:, 0], verts[:,1], faces, verts[:, 2],
                         cmap=cmap, alpha = al, antialiased=False, linewidth=0)
-        
+        # plot the Earth in the origin
+        from matplotlib.offsetbox import AnnotationBbox, OffsetImage
+        from matplotlib._png import read_png
+        im = read_png('earth.png')
+        imagebox = OffsetImage(im, zoom=.05)
+        xy = [0.0, 0.0]               # coordinates to position this image
+
+        ab = AnnotationBbox(imagebox, xy,
+                            xybox=(0., 0.),
+                            xycoords='data',
+                            boxcoords="offset points",
+                            frameon=False)
+        ax.add_artist(ab)
+#        ax.scatter([0.0],[0.0],[0.0],c='k',s=200,marker=r'$\bigoplus$',edgecolors='none')
         ax.grid(False)
         ax.xaxis.pane.set_edgecolor('black')
         ax.yaxis.pane.set_edgecolor('black')
@@ -762,7 +782,10 @@ if __name__=='__main__':
         ax.zaxis._axinfo['tick']['inward_factor'] = 0
         ax.zaxis._axinfo['tick']['outward_factor'] = 0.4
         ax.zaxis._axinfo['tick']['outward_factor'] = 0.4
-        
+
+        ax.set_xlim(-MAX,MAX)
+        ax.set_ylim(-MAX,MAX)
+        ax.set_zlim(-MAX,MAX)
         ax.set_xlabel(r"$D_L/\mathrm{Mpc}$")
         ax.set_ylabel(r"$D_L/\mathrm{Mpc}$")
         ax.set_zlabel(r"$D_L/\mathrm{Mpc}$")
