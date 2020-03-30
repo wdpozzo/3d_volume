c845cfa3dba1284b51b7181f8a91fb899164809c
Walter Del Pozzo <walter.delpozzo@ligo.org>
2020-03-30 10:43:59 +0200

    reorganised the repository with a proper setup and installation

--- setup.py
+++ setup.py
@@ -1,33 +1,54 @@
-from setuptools import setup, find_packages
-from distutils.extension import Extension
-from Cython.Build import cythonize
 import numpy
+from setuptools import setup, find_packages
+from setuptools import Extension
+from setuptools.command.build_ext import build_ext as _build_ext
+from codecs import open
+from os import path
 
+## see https://stackoverflow.com/a/21621689/1862861 for why this is here
+class build_ext(_build_ext):
+    def finalize_options(self):
+        _build_ext.finalize_options(self)
+        # Prevent numpy from thinking it is still in its setup process:
+        __builtins__.__NUMPY_SETUP__ = False
+        import numpy
+        self.include_dirs.append(numpy.get_include())
+
+
+from Cython.Build import cythonize
 
 ext_modules=[
-             Extension("cumulative",
-                       sources=["cumulative.pyx"],
+             Extension("volume_reconstruction.utils.cumulative",
+                       sources=["volume_reconstruction/utils/cumulative.pyx"],
                        libraries=["m"], # Unix-like specific
-                       include_dirs=[numpy.get_include()]
+                       include_dirs=[numpy.get_include(),"volume_reconstruction"]
+                       ),
+             Extension("volume_reconstruction.utils.utils",
+                       sources=["volume_reconstruction/utils/utils.pyx"],
+                       libraries=["m"], # Unix-like specific
+                       include_dirs=[numpy.get_include(),"volume_reconstruction"]
                        )
              ]
-
 setup(
-      name = "Cumulative",
-      ext_modules = cythonize(ext_modules),
-      include_dirs=[numpy.get_include()]
-      )
-
-ext_modules=[
-             Extension("utils",
-                       sources=["utils.pyx"],
-                       libraries=["m"], # Unix-like specific
-                       include_dirs=[numpy.get_include()]
-                       )
-             ]
-
-setup(
-      name = "utils",
-      ext_modules = cythonize(ext_modules),
-      include_dirs=[numpy.get_include()]
-      )
+    name = "volume_reconstruction",
+    version='0.5',
+    description='gw volume reconstruction',
+    classifiers=[
+                 'Development Status :: 3 - Beta',
+                 'License :: OSI Approved :: MIT License',
+                 'Programming Language :: Python :: 3.7',
+                 'Topic :: Data Analysis :: Physics',
+                 ],
+    keywords='gravitational wave black holes neutron stars',
+    author='Walter Del Pozzo, Archisman Ghosh',
+    author_email='walter.delpozzo@ligo.org, archisman.ghosh@ligo.org',
+    license='MIT',
+    cmdclass={'build_ext': build_ext},
+    packages = find_packages(),
+    ext_modules = cythonize(ext_modules, language_level = "3"),
+    include_dirs=[numpy.get_include()],
+    install_requires=['numpy','cpnest','corner','matplotlib','setuptools-git'],
+    scripts  = ['bin/VolumeReconstruction'],
+    include_package_data=False,
+    zip_safe=False
+)
